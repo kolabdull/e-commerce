909477b2eef706e7153d49d099e5f7bed6ecbf8e
kolabdull <180402034@live.unilag.edu.ng>
2023-10-07 00:18:34 +0100

    Paystack integration

--- routes/paystack.js
+++ routes/paystack.js
@@ -1,6 +1,24 @@
-const router = require("exress").Router();
-const Paystack = require("paystack")("process.env.PAYSTACK_KEY");
+const router = require("express").Router();
 
-router.post("/payment", (req, res) => {});
+const axios = require("axios");
+
+// This is the second configuration option
+router.post("/payments", async (req, res) => {
+  console.log(req.body);
+  try {
+    const paystackResponse = await axios.post(
+      "https://api.paystack.co/transaction/initialize",
+      { email: req.body.email, amount: req.body.amount * 100 },
+      {
+        //   method: "post",
+        //   url: "https://api.paystack.co/transaction/initialize",
+        headers: { Authorization: "Bearer " + process.env.PAYSTACK_KEY },
+      }
+    );
+    res.send(paystackResponse.data);
+  } catch (err) {
+    console.log(err.response.data);
+  }
+});
 
 module.exports = router;
